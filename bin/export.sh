ebd791332c03cafdf2cf9a5748fead1830aea4cf
Alex Kozeka <kozeka.alex@gmail.com>
2004-08-09 07:53:18 +0000

    proj_name fix

--- bin/export.sh
+++ bin/export.sh
@@ -7,7 +7,7 @@ rm -r -f $TMP_DIR
 mkdir $TMP_DIR
 cd $TMP_DIR
 
-cvs export -kv -r $PROJ_RELEASE -d $PROJ_RELEASE app_skeleton/code >> export.log
+cvs export -kv -r $PROJ_RELEASE -d $PROJ_RELEASE $PROJ_NAME/code >> export.log
 
 cd $PROJ_RELEASE
 tar zcf ../$PROJ_RELEASE.tgz .
