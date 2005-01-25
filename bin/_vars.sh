29080a535f0279fd34fd9577d23bbd2edcebd73c
Alex Kozeka <kozeka.alex@gmail.com>
2005-01-25 09:19:02 +0000

    Update info for hostname in install script

--- bin/_vars.sh
+++ bin/_vars.sh
@@ -10,7 +10,7 @@ EXCLUDE_FROM_CVS_EXPORT_ALWAYS='include/_build_browse_info.bat include/.tags-aut
 EXCLUDE_FROM_CVS_EXPORT='images swf'
 
 # Hosting configuration variables for install script.
-HOST_NAME='basetre.net'
-HOST_USER='user'
-HOST_PASSWORD='password'
+HOST_NAME='domain_name'
+HOST_USER='domain_user'
+HOST_PASSWORD='domain_password'
 HOST_HOME_DIR='httpdocs'
