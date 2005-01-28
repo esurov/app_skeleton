33e66d8e394079757f45267134558d663a60164d
Alex Kozeka <kozeka.alex@gmail.com>
2005-01-28 09:02:05 +0000

    More files excluded from cvs export.

--- bin/_vars.sh
+++ bin/_vars.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Common configuration variables for cvs export scripts.
-PROJ_NAME='app_skeleton'
+PROJ_NAME='cvs_project_module_name'
 PROJ_RELEASE='HEAD'
 
 TMP_DIR='tmp'
 
-EXCLUDE_FROM_CVS_EXPORT_ALWAYS='include/_build_browse_info.bat include/.tags-autoload'
+EXCLUDE_FROM_CVS_EXPORT_ALWAYS='include/_build_browse_info.bat include/.tags-autoload config/.cvsignore include/.cvsignore log/.cvsignore'
 EXCLUDE_FROM_CVS_EXPORT='images swf'
 
 # Hosting configuration variables for install script.
