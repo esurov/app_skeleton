0a354acd9f2c00d3b75815192d3b1d8678b9af87
Alex Kozeka <kozeka.alex@gmail.com>
2006-05-03 13:54:56 +0000

    Password removed

--- bin/release/vars.sh
+++ bin/release/vars.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+# http://!!CHANGE_ME!!/
 HOST_NAME="basetre.net"
 HOST_USER="!!CHANGE_ME!!"
-HOST_PASSWORD="!!CHANGE_ME!!"
 HOST_WRITABLE_DIR="~/private"
 HOST_PROJ_PARENT_DIR="~"
 PROJ_DIR_NAME="httpdocs"
