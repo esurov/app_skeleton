d52810b9a9a35361d1c4683ac086eb6696573922
Alex Kozeka <kozeka.alex@gmail.com>
2007-02-25 07:33:59 +0000

    Added news articles management at admin area. Removed AdminApp and its templates. Guest user menu fixes.

--- code/admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once("include/_init.php");
-require_once("include/admin_app.php");
-
-$app = new AdminApp();
-$app->run();
-
-?>
--- code/include/user_app.php
+++ code/include/user_app.php
@@ -41,6 +41,11 @@ class UserApp extends CustomApp {
             // News articles
             "pg_news_articles" => $e,
             "pg_news_article_view" => $e,
+            "pg_news_article_edit" => $a,
+            "update_news_article" => $a,
+            "delete_news_article" => $a,
+            "delete_news_article_image" => $a,
+            "delete_news_article_file" => $a,
 
             // Contact form
             "pg_contact_form" => $e,
@@ -258,89 +263,6 @@ class UserApp extends CustomApp {
         $this->create_self_redirect_response(array("action" => "pg_users"));
     }
 //
-/*
-    function action_pg_news_article_edit() {
-        $templates_dir = "news_article_edit";
-
-        $news_article = get_param_value($this->action_params, "news_article", null);
-        if (is_null($news_article)) {
-            $news_article = $this->read_id_fetch_db_object("NewsArticle");
-        }
-        $news_article_edit = $this->create_object(
-            "ObjectEdit",
-            array(
-                "templates_dir" => "{$templates_dir}/news_article_edit",
-                "template_var" => "news_article_edit",
-                "obj" => $news_article,
-                "context" => "edit",
-            )
-        );
-        $news_article_edit->print_values();
-
-        $this->print_file("{$templates_dir}/body.html", "body");
-    }
-
-    function action_update_news_article() {
-        $news_article = $this->read_id_fetch_db_object("NewsArticle");
-        $news_article_old = $news_article;
-        $news_article->read();
-
-        $messages = $news_article->validate($news_article_old);
-
-        if (count($messages) != 0) {
-            $this->print_status_messages($messages);
-            $this->run_action("pg_news_article_edit", array("news_article" => $news_article));
-        } else {
-            $this->process_uploaded_image(
-                $news_article,
-                "image_id",
-                "image_file",
-                array(
-                    "image_processor.class" => $this->get_config_value("image_processor"),
-                    "image_processor.actions" => array(
-                        array(
-                            "name" => "crop_and_resize",
-                            "width" => $this->get_config_value("news_article_image_width"),
-                            "height" => $this->get_config_value("news_article_image_height"),
-                        ),
-                    ),
-                    "is_thumbnail" => 0,
-                )
-            );
-            $this->process_uploaded_image(
-                $news_article,
-                "thumbnail_image_id",
-                "image_file",
-                array(
-                    "image_processor.class" => $this->get_config_value("image_processor"),
-                    "image_processor.actions" => array(
-                        array(
-                            "name" => "crop_and_resize",
-                            "width" => $this->get_config_value(
-                                "news_article_thumbnail_image_width"
-                            ),
-                            "height" => $this->get_config_value(
-                                "news_article_thumbnail_image_height"
-                            ),
-                        ),
-                        // This is example of second image processor action
-                        // Remove if grayscale is not needed (almost always ;) )
-                        array(
-                            "name" => "convert_to_grayscale",
-                        ),
-                    ),
-                    "is_thumbnail" => 1,
-                )
-            );
-
-            $this->process_uploaded_file($news_article, "file_id", "file");
-
-            $news_article->save();
-            $this->print_status_message_db_object_updated($news_article_old);
-            $this->create_self_redirect_response(array("action" => "pg_news_articles"));
-        }
-    }
-*/
     function action_pg_login() {
         $templates_dir = "login";
 
@@ -595,19 +517,28 @@ class UserApp extends CustomApp {
 //
     function action_pg_news_articles() {
         $templates_dir = "news_articles";
+
+        $user_role = $this->get_user_role();
+        if ($user_role == "admin") {
+            $templates_subdir = "news_articles_admin";
+            $context = "list_item_admin";
+        } else {
+            $templates_subdir = "news_articles";
+            $context = "list_item";
+        }
         
         $news_article = $this->create_db_object("NewsArticle");
         $news_articles_list = $this->create_object(
             "PagedQueryObjectsList",
             array(
-                "templates_dir" => "{$templates_dir}/news_articles",
+                "templates_dir" => "{$templates_dir}/{$templates_subdir}",
                 "template_var" => "news_articles",
                 "obj" => $news_article,
                 "query_ex" => array(
                     "order_by" => "created DESC, id DESC",
                 ),
                 "filter_form.visible" => true,
-                "context" => "list_item",
+                "context" => $context,
             )
         );
         $news_articles_list->print_values();
@@ -690,7 +621,6 @@ class UserApp extends CustomApp {
         $email_sender->Send();
     }
 //
-
     function action_pg_users() {
         $templates_dir = "users";
         
@@ -709,6 +639,122 @@ class UserApp extends CustomApp {
         $users_list->print_values();
 
         $this->print_file("{$templates_dir}/body.html", "body");
+    }
+//
+    function action_pg_news_article_edit() {
+        $templates_dir = "news_article_edit";
+
+        $news_article = get_param_value($this->action_params, "news_article", null);
+        if (is_null($news_article)) {
+            $news_article = $this->read_id_fetch_db_object("NewsArticle");
+        }
+        $news_article_edit = $this->create_object(
+            "ObjectEdit",
+            array(
+                "templates_dir" => "{$templates_dir}/news_article_edit",
+                "template_var" => "news_article_edit",
+                "obj" => $news_article,
+                "context" => "edit",
+            )
+        );
+        $news_article_edit->print_values();
+
+        $this->print_file("{$templates_dir}/body.html", "body");
+    }
+
+    function action_update_news_article() {
+        $news_article = $this->read_id_fetch_db_object("NewsArticle");
+        $news_article_old = $news_article;
+        $news_article->read();
+
+        $messages = $news_article->validate($news_article_old);
+
+        if (count($messages) != 0) {
+            $this->print_status_messages($messages);
+            $this->run_action("pg_news_article_edit", array("news_article" => $news_article));
+        } else {
+            $this->process_uploaded_image(
+                $news_article,
+                "image_id",
+                "image_file",
+                array(
+                    "image_processor.class" => $this->get_config_value("image_processor"),
+                    "image_processor.actions" => array(
+                        array(
+                            "name" => "crop_and_resize",
+                            "width" => $this->get_config_value("news_article_image_width"),
+                            "height" => $this->get_config_value("news_article_image_height"),
+                        ),
+                    ),
+                    "is_thumbnail" => 0,
+                )
+            );
+            $this->process_uploaded_image(
+                $news_article,
+                "thumbnail_image_id",
+                "image_file",
+                array(
+                    "image_processor.class" => $this->get_config_value("image_processor"),
+                    "image_processor.actions" => array(
+                        array(
+                            "name" => "crop_and_resize",
+                            "width" => $this->get_config_value(
+                                "news_article_thumbnail_image_width"
+                            ),
+                            "height" => $this->get_config_value(
+                                "news_article_thumbnail_image_height"
+                            ),
+                        ),
+                        // This is example of second image processor action
+                        // Remove if grayscale is not needed (almost always ;) )
+                        array(
+                            "name" => "convert_to_grayscale",
+                        ),
+                    ),
+                    "is_thumbnail" => 1,
+                )
+            );
+
+            $this->process_uploaded_file($news_article, "file_id", "file");
+
+            $news_article->save();
+            $this->print_status_message_db_object_updated($news_article_old);
+            $this->create_self_redirect_response(array("action" => "pg_news_articles"));
+        }
+    }
+
+    function action_delete_news_article() {
+        $news_article = $this->read_id_fetch_db_object("NewsArticle");
+
+        $this->delete_db_object(array(
+            "obj" => $news_article,
+            "success_url_params" => array("action" => "pg_news_articles"),
+            "error_url_params" => array("action" => "pg_news_articles"),
+        ));
+    }
+
+    function action_delete_news_article_image() {
+        $news_article = $this->read_id_fetch_db_object("NewsArticle");
+
+        $this->delete_db_object_image($news_article, "image_id");
+        
+        $this->add_session_status_message(new OkStatusMsg("news_article_image_deleted"));
+        $this->create_self_redirect_response(array(
+            "action" => "pg_news_article_edit",
+            "news_article_id" => $news_article->id,
+        ));
+    }
+
+    function action_delete_news_article_file() {
+        $news_article = $this->read_id_fetch_db_object("NewsArticle");
+
+        $this->delete_db_object_file($news_article, "file_id");
+        
+        $this->add_session_status_message(new OkStatusMsg("news_article_file_deleted"));
+        $this->create_self_redirect_response(array(
+            "action" => "pg_news_article_edit",
+            "news_article_id" => $news_article->id,
+        ));
     }
 
 }
